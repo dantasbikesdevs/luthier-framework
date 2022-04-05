0be56d25666fbe710689a64917fbd29f898fb091
lucas rego <lucasalvesrego.contato@gmail.com>
2022-04-05 09:52:38 -0300

    feat: testes restantes para xml

--- tests/Unit/XmlTest.php
+++ tests/Unit/XmlTest.php
@@ -5,21 +5,21 @@ use Luthier\Xml\XmlParser;
 use function PHPUnit\Framework\assertEquals;
 
 test("passagem de xml para array associativo", function () {
-  $xml = "
-    <pai xmlns=\"http://www.pai.com.br/schemas/pai\">
-      <filho id=\"12\">
-        <neto>
-          <idade>1</idade>
-          <nome>Lorem</nome>
-        </neto>
-        <neto>
-          <nome>Ipsum</nome>
-        </neto>
-      </filho>
-      <nome>Dolor</nome>
-    </pai>";
+  $xml = '
+  <pai xmlns="http://www.pai.com.br/schemas/pai">
+    <filho id="12">
+      <neto>
+        <idade>1</idade>
+        <nome>Lorem</nome>
+      </neto>
+      <neto>
+        <nome>Ipsum</nome>
+      </neto>
+    </filho>
+    <nome>Dolor</nome>
+  </pai>';
 
-  $resultadoEsperado = [
+  $expectedArray = [
     "filho" => [
       "@attributes" => ["id" => "12"],
       "neto" => [
@@ -30,7 +30,61 @@ test("passagem de xml para array associativo", function () {
     "nome" => "dolor"
   ];
 
-  $resultado = XmlParser::toArray($xml);
+  $result = XmlParser::toArray($xml);
 
-  assertEquals($resultadoEsperado, $resultado);
+  assertEquals($expectedArray, $result);
+});
+
+
+test("tranformação de xml em json", function () {
+  $xml = '
+  <pai xmlns="http://www.pai.com.br/schemas/pai">
+    <filho id="12">
+      <neto>
+        <idade>1</idade>
+        <nome>Lorem</nome>
+      </neto>
+      <neto>
+        <nome>Ipsum</nome>
+      </neto>
+    </filho>
+    <nome>Dolor</nome>
+  </pai>';
+
+  $expectedJson = '{"filho":{"@attributes":{"id":"12"},"neto":[{"idade":"1","nome":"lorem"},{"nome":"ipsum"}]},"nome":"dolor"}';
+
+  $result = XmlParser::toJson($xml);
+
+  assertEquals($expectedJson, $result);
+});
+
+test("transformação de array em xml", function () {
+  $array = [
+    "filho" => [
+      "Attr_id" => 12,
+      "neto" => [
+        ["idade" => 1, "nome" => "lorem"],
+        ["nome" => "ipsum"],
+      ],
+    ],
+    "nome" => "dolor"
+  ];
+
+  $expectedXml =
+    '<?xml version="1.0" encoding="utf-8"?>
+<filho id="12">
+  <neto>
+    <idade>1</idade>
+    <nome>lorem</nome>
+  </neto>
+  <neto>
+    <nome>ipsum</nome>
+  </neto>
+</filho>
+<nome>dolor</nome>
+';
+
+  $result = XmlParser::xmlEncode($array, "utf-8");
+
+  assertEquals($expectedXml, $result);
 });
