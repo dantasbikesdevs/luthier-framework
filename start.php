1c58b960fbd101e76c9784b7e52538ca90dab1b6
lucas rego <lucasalvesrego.contato@gmail.com>
2022-05-23 11:11:32 -0300

    fix(hot): localizacao do autoload

--- start.php
+++ start.php
@@ -1,6 +1,6 @@
 <?php
 // Prossegue com a execução do instalador
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/../../autoload.php";
 
 use Luthier\Cli\Colors\Colors;
 use Luthier\Cli\Cli;
