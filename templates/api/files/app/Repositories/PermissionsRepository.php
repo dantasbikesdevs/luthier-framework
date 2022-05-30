18216285d54b08c7b80462d4eeefe822ef58198c
andre <andre.neres@dantaseletro.com.br>
2022-05-30 10:41:13 -0300

    refactor: retorno apenas das colunas da tabela do repositorio

--- templates/api/files/app/Repositories/PermissionsRepository.php
+++ templates/api/files/app/Repositories/PermissionsRepository.php
@@ -68,7 +68,7 @@ class PermissionsRepository extends Repository
 
   public function findByUser(UserEntity $user){
     $id = $user->getId();
-    return $this->queryBuilder->select("DISTINCT *")
+    return $this->queryBuilder->select("p.*")
       ->from("$this->tableName p")
       ->innerJoinWith("$this->tableRelation up", "up.ID_PERMISSION = p.ID")
       ->where("up.ID_USER = |$id|")
