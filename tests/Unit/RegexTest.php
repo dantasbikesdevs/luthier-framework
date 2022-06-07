e3d42ea75d1ad2de898a5e7e9434dbe5da37784b
andre <andre.neres@dantaseletro.com.br>
2022-06-07 17:47:19 -0300

    refactor: alterando it para test

--- tests/Unit/RegexTest.php
+++ tests/Unit/RegexTest.php
@@ -2,24 +2,24 @@
 
 use Luthier\Regex\Regex;
 
-it("verifica se a assinatura é muito fraca", function ($signature) {
+test("verifica se a assinatura é muito fraca", function ($signature) {
   $this->assertDoesNotMatchRegularExpression(Regex::$strongSignature, $signature);
 })->with([
   "broken_signatures"
 ]);
 
-it("verifica se a senha é muito fraca", function ($password) {
+test("verifica se a senha é muito fraca", function ($password) {
   $this->assertDoesNotMatchRegularExpression(Regex::$strongPassword, $password);
 })->with([
   "broken_passwords"
 ]);
 
-it("verifica se os e-mails são válidos", function ($email) {
+test("verifica se os e-mails são válidos", function ($email) {
   $this->assertDoesNotMatchRegularExpression(Regex::$validEmail, $email);
 })->with([
   "invalid_emails"
 ]);
 
-it("verifica se há espaços duplicados na string", function () {
+test("verifica se há espaços duplicados na string", function () {
   $this->assertMatchesRegularExpression(Regex::$contiguousBlankSpaces, "a   b");
 });
