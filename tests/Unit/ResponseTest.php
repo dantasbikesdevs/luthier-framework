d26b31434fa98915bf309ebbf94e9430f2dacf98
andre <andre.neres@dantaseletro.com.br>
2022-06-08 11:37:23 -0300

    feat: testes da classe response

---
+++ tests/Unit/ResponseTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Luthier\Http\Response;
+
+test("deve criar um objeto de resposta corretamente", function () {
+  $response = new Response("Teste realizado com sucesso.", 200);
+
+  expect($response->getContent())
+    ->toBe("Teste realizado com sucesso.");
+
+  expect($response->getCode())
+    ->toBe(200);
+
+  expect($response->getContentType())
+    ->toBe("application/json");
+});
+
+test("deve enviar uma resposta corretamente", function () {
+  $response = new Response();
+
+  $result = $response->send("Teste realizado com sucesso.");
+
+  expect($result)
+    ->getContent()
+    ->toBe(["message" => "Teste realizado com sucesso."])
+    ->getCode()
+    ->toBe(200);
+});
