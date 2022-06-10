22ac983b3a2fbed6d4c558993364cda0c9b8ebd8
andre <andre.neres@dantaseletro.com.br>
2022-06-10 16:41:10 -0300

    refactor: mensagem de erro ao validar parametros obrigatorios

--- src/Utils/Validate.php
+++ src/Utils/Validate.php
@@ -11,7 +11,7 @@ class Validate {
   public static function paramsRequired(array $params, int $code = 400): void {
     foreach ($params as $key => $param) {
       if (isset($param) && (!empty($param) && $param != 0)) continue;
-      throw new InvalidArgumentException("O parâmetro $key não foi definido.", $code);
+      throw new InvalidArgumentException("Parâmetro obrigatório não foi informado.", $code);
     }
   }
 }
