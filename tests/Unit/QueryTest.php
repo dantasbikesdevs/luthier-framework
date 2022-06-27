10664e845293b8ba0c6cf34b9dd457bb80b3731a
andre <andre.neres@dantaseletro.com.br>
2022-06-27 11:04:20 -0300

    test: teste do método de filtros SQL

--- tests/Unit/QueryTest.php
+++ tests/Unit/QueryTest.php
@@ -22,3 +22,31 @@ test("deve retornar uma query para consulta no banco", function () {
   expect($statement["query"])
     ->toBe("SELECT * FROM USERS u INNER JOIN USERS_GROUPS ug ON u.ID = ug.USER_ID WHERE u.ID = ? OR u.NAME = ?");
 });
+
+test("deve retornar uma query SQL com os filtros passados", function () {
+  $stub = $this->createStub(Database::class);
+
+  $queryBuilder = new Query($stub);
+
+  $id = 1;
+  $name = "André Oliveira";
+  $age = 19;
+
+  $filters = [
+    "ID <> |$id|",
+    "NAME" => $name,
+    "AGE > |$age|"
+  ];
+
+  $query = $queryBuilder->select("*")
+    ->from("USERS u")
+    ->filterWhere($filters);
+
+  $statement = $query->getSql();
+
+  expect($statement["query"])
+    ->toBe("SELECT * FROM USERS u WHERE (ID <> ? AND NAME = ? AND AGE > ?)");
+
+  expect($statement["values"])
+    ->toBe(["1", "André Oliveira", "19"]);
+});
