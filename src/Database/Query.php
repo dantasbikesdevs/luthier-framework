63194b4b1c71e7cc02c34d76dadafb875503f556
andre <andre.neres@dantaseletro.com.br>
2022-07-22 16:20:24 -0300

    fix: problemas com caracteres especiais

--- src/Database/Query.php
+++ src/Database/Query.php
@@ -435,7 +435,7 @@ class Query
     /**
      * Regex que identifica valores dentro de "|".
      */
-    $patternVariable = '/\|(?=[A-Za-z0-9])(.*?)\|/';
+    $patternVariable = '/\|(?=[^x])(.*?)\|/';
 
     /**
      * Utiliza o regex anterior para separar os valores da query
