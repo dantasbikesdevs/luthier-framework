9c42672bddc59edb78c869ad893bd63fc6f662d4
andre <andre.neres@dantaseletro.com.br>
2023-01-24 11:45:31 -0300

    refactor: altera tipo do retorno para a interface de Route ao invés da classe concreta

--- src/Http/Router/Contracts/Router.php
+++ src/Http/Router/Contracts/Router.php
@@ -4,43 +4,45 @@ declare(strict_types=1);
 
 namespace Luthier\Http\Router\Contracts;
 
+use Luthier\Http\Router\Contracts\Route as RouteInterface;
+
 interface Router
 {
     /**
      * Método responsável por registrar uma nova rota GET
      * e retornar o seu registro para demais manipulações.
      */
-    public static function get(string $uri): Route;
+    public static function get(string $uri): RouteInterface;
 
     /**
      * Método responsável por registrar uma nova rota POST
      * e retornar o seu registro para demais manipulações.
      */
-    public static function post(string $uri): Route;
+    public static function post(string $uri): RouteInterface;
 
     /**
      * Método responsável por registrar uma nova rota GET
      * e retornar o seu registro para demais manipulações.
      */
-    public static function put(string $uri): Route;
+    public static function put(string $uri): RouteInterface;
 
     /**
      * Método responsável por registrar uma nova rota PATCH
      * e retornar o seu registro para demais manipulações.
      */
-    public static function patch(string $uri): Route;
+    public static function patch(string $uri): RouteInterface;
 
     /**
      * Método responsável por registrar uma nova rota DELETE
      * e retornar o seu registro para demais manipulações.
      */
-    public static function delete(string $uri): Route;
+    public static function delete(string $uri): RouteInterface;
 
     /**
      * Método responsável por registrar um grupo de rotas e
      * retornar o seu registro para demais manipulações.
      */
-    public static function prefix(string $prefix): Route;
+    public static function prefix(string $prefix): RouteInterface;
 
     /**
      * Método responsável por executar o roteador, executando
