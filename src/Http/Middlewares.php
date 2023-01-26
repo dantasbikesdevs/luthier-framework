ba57b9a6a3926feedc4527f1f8d38aa3b2090dcc
andre <andre.neres@dantaseletro.com.br>
2023-01-26 17:28:47 -0300

    fix: corrige nível do diretório de middlewares do projeto

--- src/Http/Middlewares.php
+++ src/Http/Middlewares.php
@@ -55,7 +55,7 @@ class Middlewares
      */
     private static function mapMiddlewaresApplication(): void
     {
-        $path = realpath(dirname(__DIR__, 2)) . "/" . self::$path;
+        $path = realpath(dirname(__DIR__, 5)) . "/" . self::$path;
 
         $dir = dir($path);
 
